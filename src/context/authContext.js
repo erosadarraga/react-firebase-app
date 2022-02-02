103f5ac8bd46543eca87fbed8eb36ca919e6528f
Eros Jose Adarraga Jimenez <eroslbac123456@gmail.com>
2022-02-02 15:01:36 -0500

    m

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,34 +1,24 @@
-import { createContext, useContext, useEffect } from 'react'
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  onAuthStateChanged,
-} from 'firebase/auth'
-import { auth } from '../firebase'
-export const authContext = createContext()
+import { createContext, useContext, useEffect } from 'react';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../firebase';
+export const authContext = createContext();
 
 export const useAuth = () => {
-  const context = useContext(authContext)
-  if (!context) throw new Error('There is no auth provider ')
-  return context
-}
+	const context = useContext(authContext);
+	if (!context) throw new Error('There is no auth provider ');
+	return context;
+};
 
 export function AuthProvider({ children }) {
-  const signup = (email, password) =>
-    createUserWithEmailAndPassword(auth, email, password)
+	const signup = (email, password) => createUserWithEmailAndPassword(auth, email, password);
 
-  const login = async (email, password) =>
-    signInWithEmailAndPassword(auth, email, password)
+	const login = async (email, password) => signInWithEmailAndPassword(auth, email, password);
 
-  useEffect(() => {
-    onAuthStateChanged(auth, (currentUser) => {
-      console.log(currentUser)
-    })
-  }, [])
+	useEffect(() => {
+		onAuthStateChanged(auth, (currentUser) => {
+			console.log(currentUser);
+		});
+	}, []);
 
-  return (
-    <authContext.Provider value={{ signup, login }}>
-      {children}
-    </authContext.Provider>
-  )
+	return <authContext.Provider value={{ signup, login }}>{children}</authContext.Provider>;
 }
